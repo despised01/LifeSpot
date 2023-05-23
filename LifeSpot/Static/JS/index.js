21e63032e6d8ffa915fe97d90e7cb869863fc20f
Антон Левин <despised01@gmail.com>
2023-05-23 22:04:59 +0300

    исправление опечаток, подключение фунуций в 'index.html'

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -34,7 +34,7 @@ function checkAge() {
         alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + startDate);
     }
     else {
-        alert("Наши трансляции не предназначены для лиц моложе 18 лет. ВыL будете перенаправлены");
+        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
         window.location.href = "http://www.google.com"
     }
     return age;
